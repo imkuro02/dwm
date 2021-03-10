6efb9300a5c717cac55189b66579a0773fd04c25
imkuro02 <ignacykurowski@tutanota.com>
2021-03-10 21:53:47 +0100

    info

--- autostart.sh
+++ autostart.sh
@@ -2,6 +2,6 @@ feh --bg-scale $HOME/Pictures/bg.png
 while true; do
 	vol=$( amixer -D pulse get Master | grep 'Left:' | awk -F'[][]' '{ print $2 }')
 	echo "${vol}"
-	xsetroot -name "[$(acpi)][VOL: ${vol}][$( date +"%F %R" )]"
+	xsetroot -name "[VOL: ${vol}][$( date +"%F %R" )]"
 	sleep .1
 done
